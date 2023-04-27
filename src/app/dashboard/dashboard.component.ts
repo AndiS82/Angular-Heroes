a96b16d061385ee01afa6d490704c0bf1cfb0883
Andreas Schneider <xichtus@MacBook-Pro-von-Andreas.fritz.box>
2023-04-27 12:39:48 +0200

    done

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,12 +12,12 @@ export class DashboardComponent implements OnInit {
   constructor(private heroService: HeroService) { }
 
 ngOnInit(): void {
-  this.getHeroes()
+  this.getHeroes();
 }
 
   getHeroes() {
     this.heroService.getHeroes()
-    .subscribe(heroes=> this.heroes = heroes.slice(1,5))
-}
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+  }
 
 }
--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -11,7 +11,8 @@ import { OnInit } from '@angular/core';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit{
-  @Input() hero?: Hero;
+  // @Input() hero?: Hero;
+  hero: Hero | undefined;
 
   constructor(
     private route: ActivatedRoute,
@@ -24,12 +25,19 @@ export class HeroDetailComponent implements OnInit{
   }
 
   getHero():void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!,10);
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
 
   goBack() {
     this.location.back();
+  }
+
+  save() {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+      .subscribe(()=> this.goBack())
+    }
   }
 }
